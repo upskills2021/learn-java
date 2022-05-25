1945ad41443d6d1ce0809998afe36b8e346e7ca7
sourav <info.upskills2021@gmail.com>
2022-05-25 09:07:51 +0530

    Implement Encapsulation in CoreJava

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,11 @@
+import com.learnjava.example.encapsulation.Country;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Country country = new Country();
+        country.setName("India");
+        country.setCurrency("INR");
+        String countryInformation = country.toString();
+        System.out.println("Country Information :: " + countryInformation);
     }
 }
---
+++ src/main/java/com/learnjava/example/encapsulation/Country.java
@@ -0,0 +1,30 @@
+package com.learnjava.example.encapsulation;
+
+public class Country {
+    private String name;
+    private String currency;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
